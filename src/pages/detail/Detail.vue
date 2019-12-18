fc676752b5c01c86b2558e15d123e8e0589daf99
frankma007 <1015630728@qq.com>
2019-12-18 22:18:24 +0800

    详情页头部渐隐渐显效果

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,16 +1,22 @@
 <template>
-	<div>
-    detail
+	<div>   
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
 	</div>
 </template>
 <script>
 import DetailBanner from'./components/Banner.vue'
+import DetailHeader from'./components/Header.vue'
 	export default {
     name: "Detail",
     components:{
-      DetailBanner
+      DetailBanner,
+      DetailHeader
     }
 	};
 </script>
-<style scoped></style>
+<style lang="stylus" scoped>
+.content
+  height:50rem
+</style>
